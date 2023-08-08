d6c32ec9e788152b31db872c4a2303f6d5a4de1d
Tipu <>
2023-08-09 01:12:56 +0600

    setting page to purchase added

--- GifMaker/Controllers/settings/SettingsVC.swift
+++ GifMaker/Controllers/settings/SettingsVC.swift
@@ -166,6 +166,9 @@ class SettingsVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
                 // Perform the segue to the "PurchaseViewController"
                 //performSegue(withIdentifier: "showPurchaseVC", sender: nil)
                 print("showpurchase")
+                let storyboard = UIStoryboard(name: "Purchase", bundle: nil)
+                let destinationVC = storyboard.instantiateViewController(withIdentifier: "purchaseVC")
+                self.navigationController?.pushViewController(destinationVC, animated: true)
             case 1:
                 // Perform the segue to the "RestoreViewController"
                 //performSegue(withIdentifier: "showRestoreVC", sender: nil)
